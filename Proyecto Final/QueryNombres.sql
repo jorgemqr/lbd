9048e4d6faa755f422f56e80f0f56e82b8bf1ebd
jorgemqr <31119454+jorgemqr@users.noreply.github.com>
2018-05-05 23:34:29 -0500

    Add files via upload

---
+++ Proyecto Final/QueryNombres.sql
@@ -0,0 +1,8 @@
+use ProyectoTM;
+
+--Erick Alejandro Campos Rivero - 1506449
+--Saúl Armando Almaguer Salazar - 1743693
+--Jaaciel Fernando Maldonado Ruiz - 1684870
+--Cesar Alejandro Galvan Solis - 1669523
+--Ricardo Jafet Ali Torres de León - 1673944
+--Jorge Mario Quintanilla Rios - 1725793
